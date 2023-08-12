ffb678b7bc3e9269408dc04b51a43fc38b63f0a4
damuz91 <damuz91@gmail.com>
2023-08-12 10:50:49 -0500

    First commit

---
+++ backend/server.rb
@@ -0,0 +1,31 @@
+require 'bundler'; Bundler.require
+require 'net/http'
+require 'json'
+require 'sinatra'
+require "sinatra/cors"
+
+set :allow_origin, "*"
+set :allow_methods, "GET"
+
+get '/weather' do
+  # https://weatherstack.com/documentation
+  content_type :json
+  base_url = 'http://api.weatherstack.com'
+  method = '/current'
+  api_key = 'f82724d029f9f2b4b71c89edbaa90c0c'
+  location = params[:location]
+
+  url = "#{base_url}#{method}?access_key=#{api_key}&query=#{location}"
+  
+  url = URI.parse(url)
+  http = Net::HTTP.new(url.host, url.port)
+  response = http.get(url.request_uri)
+  body = JSON.parse(response.body)
+  puts "-> Código: #{response.code} - Cuerpo: #{body}"
+  if response.code != '200'
+    status 500
+    { error: 'Error al obtener el clima', info: body.dig('error','info') }.to_json
+  else
+    body.to_json
+  end
+end
